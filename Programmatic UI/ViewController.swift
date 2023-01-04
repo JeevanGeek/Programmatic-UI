d6bc10f666509a84c8124948c4631818e0580efc
Jeevan Joshi <jeevanc@geekyants.com>
2023-01-04 10:15:01 +0530

    Initial Commit

---
+++ Programmatic UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Programmatic UI
+//
+//  Created by Jeevan Chandra Joshi on 04/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
